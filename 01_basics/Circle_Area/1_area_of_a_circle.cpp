51e996458de58dea6e9332dc4e70348dc1315798
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-02-20 14:33:21 +0000

    Add solution for Area of Circle

---
+++ 01_basics/Circle_Area/1_area_of_a_circle.cpp
@@ -0,0 +1,21 @@
+// Problem: Find Area of a Circle
+// Cncepts: Input/Output
+// Difficulty: Beginner
+
+#include <iostream>
+using namespace std;
+int main(){
+    float radius, area;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+      area = 3.14159 * radius * radius;
+    cout << "The area of the circle is: " << area << endl;
+    return 0;
+
+  
+
+
+
+
+
+}
